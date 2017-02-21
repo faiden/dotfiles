4a509f9cd95d85a26c934ed3774d97c3f2fab6ee
faiden <info@faiden.com>
2017-02-21 22:02:57 +0100

    updated cordinates

--- .bin/dzen-cal.sh
+++ .bin/dzen-cal.sh
@@ -9,7 +9,7 @@ WIDTH=180
 LINES=7
 
 XPOS=1093
-YPOS=941
+YPOS=836
 
 BACKGROUND="#151515"
 FOREGROUND="#D7D0C7"
@@ -37,5 +37,5 @@ TODAY=$(date +'%-d')
 
 #cal -m | sed -re "s/^(.*[A-Za-z][A-Za-z]*.*)$/^fg($HIGHLIGHT)\1^fg()/;s/(^|[ ])($TODAY)($|[ ])/\1^fg($HIGHLIGHT)\2^fg()\3/" |dzen2 -p -bg "$BACKGROUND" -fg $FOREGROUND -l "$LINES" -w "$WIDTH" -x "$XPOS" -y "$YPOS" -sa c -e 'onstart=uncollapse;button1=exit;button3=exit' -title-name 'dzen-popup-cal' -fn '-*-tewi-medium-*-*-*-*-*-*-*-*-*-*-*'
 
-cal -m | sed -re "s/^(.*[A-Za-z][A-Za-z]*.*)$/^fg($HIGHLIGHT)^bg($BACKGROUND)\1/;s/(^|[ ])($TODAY)($|[ ])/\1^bg($HLTODAY)^fg($BACKGROUND)\2^fg($FOREGROUND)^bg($BACKGROUND)\3/" |dzen2 -p -bg "$BACKGROUND" -fg $FOREGROUND -l "$LINES" -w "$WIDTH" -x "$XPOS" -y "$YPOS" -sa c -e 'onstart=uncollapse;button1=exit;button3=exit' -title-name 'dzen-popup-cal' -fn '-*-tewi-medium-*-*-*-*-*-*-*-*-*-*-*'
+cal -m | sed -re "s/^(.*[A-Za-z][A-Za-z]*.*)$/^fg($HIGHLIGHT)^bg($BACKGROUND)\1/;s/(^|[ ])($TODAY)($|[ ])/\1^bg($HLTODAY)^fg($BACKGROUND)\2^fg($FOREGROUND)^bg($BACKGROUND)\3/" |dzen2 -p -bg "$BACKGROUND" -fg $FOREGROUND -l "$LINES" -w "$WIDTH" -x "$XPOS" -y "$YPOS" -sa c -e 'onstart=uncollapse;button1=exit;button3=exit' -title-name 'dzen-popup-cal' -fn 'lucy tewi'
 
