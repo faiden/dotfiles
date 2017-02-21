0bd92a245f2911b9c3c2ce6cbde9144908ef9b8f
faiden <info@faiden.com>
2017-02-21 22:03:15 +0100

    updated cordinates

--- .bin/img_popup.sh
+++ .bin/img_popup.sh
@@ -2,7 +2,7 @@
 case $1 in
 "popup")
 bspc rule -a feh -o desktop='^5'
-feh --scale-down -N --reload 60 -Z -x -B black -g 320x240+500+812 http://87.237.210.145/~checkpoi/webcam/borasenergi/boras_1_1280.jpg &
+feh --scale-down -N --reload 60 -Z -x -B black -g 320x240+500+756 http://87.237.210.145/~checkpoi/webcam/borasenergi/boras_1_1280.jpg &
 ;;
 "kill")
 kill $(pidof feh)
