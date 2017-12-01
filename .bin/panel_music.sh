162ca6668f2ee22f6c23c5dc593266f6a090cb27
faiden <info@faiden.com>
2017-12-01 14:38:05 +0100

    Just a tweak

--- .bin/panel_music.sh
+++ .bin/panel_music.sh
@@ -9,13 +9,17 @@ read -r MPC_TITLE <<<"$MPC"
 # Extract the title
 	MPC_TITLE=${MPC_TITLE//Ã¥/å} #Slay radio bad characters fix
 	MPC_TITLE=${MPC_TITLE//Ã¤/ä} # --
+	MPC_TITLE=${MPC_TITLE//Ãµ/õ} # --
 	MPC_TITLE=${MPC_TITLE//Ã¶/ö} # --
+	MPC_TITLE=${MPC_TITLE//Ã/Ö} # --
 	MPC_TITLE=${MPC_TITLE//Ã±/ñ} # --
 	MPC_TITLE=${MPC_TITLE//Ã©/é} # --
 	MPC_TITLE=${MPC_TITLE//Ã³/ó} # --
 	MPC_TITLE=${MPC_TITLE//Ãµ/õ} # --
 	MPC_TITLE=${MPC_TITLE//Ã¡/á} # --
+	MPC_TITLE=${MPC_TITLE//Ã¡/á} # --
 	MPC_TITLE=${MPC_TITLE//Ã¼/ü} # --
+	MPC_TITLE=${MPC_TITLE//Ö©/é} # --
 	MPC_TITLE=${MPC_TITLE#*'Now playing: '}
 	MPC_TITLE=${MPC_TITLE%%'- Join us on IRC'*}
 case "$MPC" in
