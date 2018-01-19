aa1c68a55541e2b8ab4317417e6c8f1fe7e76f8d
faiden <info@faiden.com>
2018-01-19 15:40:50 +0100

    added % progression based background to the song title

--- .bin/panel_music.sh
+++ .bin/panel_music.sh
@@ -1,33 +1,59 @@
 #!/bin/bash
 
+# Lemonbar music title/progress script
+# Shows the current song that mpd is playing with status icons and progression % background of the song title.
+
 . panel_colors
 
-IP="192.168.0.3"
-PORT="6600"
-MPC=$(mpc -h $IP -p $PORT 2>/dev/null)
-read -r MPC_TITLE <<<"$MPC"
-# Extract the title
-	MPC_TITLE=${MPC_TITLE//Ã¥/å} #Slay radio bad characters fix
-	MPC_TITLE=${MPC_TITLE//Ã¤/ä} # --
-	MPC_TITLE=${MPC_TITLE//Ãµ/õ} # --
-	MPC_TITLE=${MPC_TITLE//Ã¶/ö} # --
-	MPC_TITLE=${MPC_TITLE//Ã/Ö} # --
-	MPC_TITLE=${MPC_TITLE//Ã±/ñ} # --
-	MPC_TITLE=${MPC_TITLE//Ã©/é} # --
-	MPC_TITLE=${MPC_TITLE//Ã³/ó} # --
-	MPC_TITLE=${MPC_TITLE//Ãµ/õ} # --
-	MPC_TITLE=${MPC_TITLE//Ã¡/á} # --
-	MPC_TITLE=${MPC_TITLE//Ã¡/á} # --
-	MPC_TITLE=${MPC_TITLE//Ã¼/ü} # --
-	MPC_TITLE=${MPC_TITLE//Ö©/é} # --
-	MPC_TITLE=${MPC_TITLE#*'Now playing: '}
-	MPC_TITLE=${MPC_TITLE%%'- Join us on IRC'*}
-case "$MPC" in
+# Get values from mpd 
+_ip="192.168.0.3"
+_port="6600"
+_mpc=$(mpc -h $_ip -p $_port 2>/dev/null)
+
+# Sort the values up i arrays
+IFS=$'\n' read -ra _mpc -d '' <<<"$_mpc"
+
+# Get the song title
+_mpc_title=${_mpc[0]}
+
+# Get the Current progress of the song
+_mpc_progress="${_mpc[1]}"
+_mpc_progress="${_mpc_progress#*(}"
+_mpc_progress="${_mpc_progress%\%*}"
+
+# Slay radio locale fix (lazy)
+_mpc_title=${_mpc_title//Ã¥/å}
+_mpc_title=${_mpc_title//Ã¤/ä} 
+_mpc_title=${_mpc_title//Ãµ/õ}
+_mpc_title=${_mpc_title//Ã¶/ö}
+_mpc_title=${_mpc_title//Ã/Ö}
+_mpc_title=${_mpc_title//Ã±/ñ}
+_mpc_title=${_mpc_title//Ã©/é}
+_mpc_title=${_mpc_title//Ã³/ó}
+_mpc_title=${_mpc_title//Ãµ/õ}
+_mpc_title=${_mpc_title//Ã¡/á}
+_mpc_title=${_mpc_title//Ã¡/á}
+_mpc_title=${_mpc_title//Ã¼/ü}
+_mpc_title=${_mpc_title//Ö©/é}
+_mpc_title=${_mpc_title#*'Now playing: '}
+_mpc_title=${_mpc_title%%'- Join us on IRC'*}
+
+# Make sure that the lenght of the song title does not go above 66. The song title aligned to left and the rest right.
+# 66 is usually the most characters that I can show of the song.
+if [[ ${#_mpc_title} -le "66" ]]; then
+	_title_progress="$(($((${#_mpc_title}*${_mpc_progress}))/100))"
+else
+	_title_progress="$(($((66*${_mpc_progress}))/100))"
+fi
+
+# Check if mpd is playing/paused etc and output the title/progression as background color behind the current title.
+case "${_mpc[1]}" in
 	*\[playing\]*)
-		printf "%s" "\r ▶ ${CYAN}$MPC_TITLE%{F-}"
+		#printf "%s" "\r ▶ $_mpc_title%{F-}"
+		 printf "%s" "\r ▶ %{B#373b41}${_mpc_title:0:$_title_progress}""%{B-}""${_mpc_title:$_title_progress}%{F-}"
 		;;
 	*\[paused\]*)
-		printf "%s" "\r ▮▮ ${CYAN}$MPC_TITLE%{F-}"
+		printf "%s" "\r ▮▮ %{B#373b41}${_mpc_title:0:$_title_progress}""%{B-}""${_mpc_title:$_title_progress}%{F-}"
 		;;
 	volume*)
 		printf "%s" "\r ◾"	
